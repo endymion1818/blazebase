7d6286c09483960ba3a77171c315cf771d626c76
Ben Read <endymion1818@gmail.com>
2016-02-29 09:34:26 +0000

    nav toggle animation, navbar walker

--- functions.php
+++ functions.php
@@ -99,6 +99,215 @@ function first_and_last_menu_class($items) {
 	}
 	add_filter('wp_nav_menu_objects', 'first_and_last_menu_class');
 
+
+
+  /**
+   * Class Name: wp_bootstrap_navwalker
+   * GitHub URI: https://github.com/twittem/wp-bootstrap-navwalker
+   * Description: A custom WordPress nav walker class to implement the Bootstrap 3 navigation style in a custom theme using the WordPress built in menu manager.
+   * Version: 2.0.4
+   * Author: Edward McIntyre - @twittem
+   * License: GPL-2.0+
+   * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+   */
+
+  class wp_bootstrap_navwalker extends Walker_Nav_Menu {
+
+  	/**
+  	 * @see Walker::start_lvl()
+  	 * @since 3.0.0
+  	 *
+  	 * @param string $output Passed by reference. Used to append additional content.
+  	 * @param int $depth Depth of page. Used for padding.
+  	 */
+  	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+  		$indent = str_repeat( "\t", $depth );
+  		$output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
+  	}
+
+  	/**
+  	 * @see Walker::start_el()
+  	 * @since 3.0.0
+  	 *
+  	 * @param string $output Passed by reference. Used to append additional content.
+  	 * @param object $item Menu item data object.
+  	 * @param int $depth Depth of menu item. Used for padding.
+  	 * @param int $current_page Menu item ID.
+  	 * @param object $args
+  	 */
+  	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+  		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+  		/**
+  		 * Dividers, Headers or Disabled
+  		 * =============================
+  		 * Determine whether the item is a Divider, Header, Disabled or regular
+  		 * menu item. To prevent errors we use the strcasecmp() function to so a
+  		 * comparison that is not case sensitive. The strcasecmp() function returns
+  		 * a 0 if the strings are equal.
+  		 */
+  		if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
+  			$output .= $indent . '<li role="presentation" class="divider">';
+  		} else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
+  			$output .= $indent . '<li role="presentation" class="divider">';
+  		} else if ( strcasecmp( $item->attr_title, 'dropdown-header') == 0 && $depth === 1 ) {
+  			$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
+  		} else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
+  			$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
+  		} else {
+
+  			$class_names = $value = '';
+
+  			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+  			$classes[] = 'menu-item-' . $item->ID;
+
+  			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+
+  			if ( $args->has_children )
+  				$class_names .= ' dropdown';
+
+  			if ( in_array( 'current-menu-item', $classes ) )
+  				$class_names .= ' active';
+
+  			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+  			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+  			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+  			$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+  			$atts = array();
+  			$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+  			$atts['target'] = ! empty( $item->target )	? $item->target	: '';
+  			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
+
+  			// If item has_children add atts to a.
+  			if ( $args->has_children && $depth === 0 ) {
+  				$atts['href']   		= '#';
+  				$atts['data-toggle']	= 'dropdown';
+  				$atts['class']			= 'dropdown-toggle';
+  				$atts['aria-haspopup']	= 'true';
+  			} else {
+  				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+  			}
+
+  			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+  			$attributes = '';
+  			foreach ( $atts as $attr => $value ) {
+  				if ( ! empty( $value ) ) {
+  					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+  					$attributes .= ' ' . $attr . '="' . $value . '"';
+  				}
+  			}
+
+  			$item_output = $args->before;
+
+  			/*
+  			 * Glyphicons
+  			 * ===========
+  			 * Since the the menu item is NOT a Divider or Header we check the see
+  			 * if there is a value in the attr_title property. If the attr_title
+  			 * property is NOT null we apply it as the class name for the glyphicon.
+  			 */
+  			if ( ! empty( $item->attr_title ) )
+  				$item_output .= '<a'. $attributes .'><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
+  			else
+  				$item_output .= '<a'. $attributes .'>';
+
+  			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+  			$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
+  			$item_output .= $args->after;
+
+  			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  		}
+  	}
+
+  	/**
+  	 * Traverse elements to create list from elements.
+  	 *
+  	 * Display one element if the element doesn't have any children otherwise,
+  	 * display the element and its children. Will only traverse up to the max
+  	 * depth and no ignore elements under that depth.
+  	 *
+  	 * This method shouldn't be called directly, use the walk() method instead.
+  	 *
+  	 * @see Walker::start_el()
+  	 * @since 2.5.0
+  	 *
+  	 * @param object $element Data object
+  	 * @param array $children_elements List of elements to continue traversing.
+  	 * @param int $max_depth Max depth to traverse.
+  	 * @param int $depth Depth of current element.
+  	 * @param array $args
+  	 * @param string $output Passed by reference. Used to append additional content.
+  	 * @return null Null on failure with no changes to parameters.
+  	 */
+  	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+          if ( ! $element )
+              return;
+
+          $id_field = $this->db_fields['id'];
+
+          // Display this element.
+          if ( is_object( $args[0] ) )
+             $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+
+          parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+      }
+
+  	/**
+  	 * Menu Fallback
+  	 * =============
+  	 * If this function is assigned to the wp_nav_menu's fallback_cb variable
+  	 * and a manu has not been assigned to the theme location in the WordPress
+  	 * menu manager the function with display nothing to a non-logged in user,
+  	 * and will add a link to the WordPress menu manager if logged in as an admin.
+  	 *
+  	 * @param array $args passed from the wp_nav_menu function.
+  	 *
+  	 */
+  	public static function fallback( $args ) {
+  		if ( current_user_can( 'manage_options' ) ) {
+
+  			extract( $args );
+
+  			$fb_output = null;
+
+  			if ( $container ) {
+  				$fb_output = '<' . $container;
+
+  				if ( $container_id )
+  					$fb_output .= ' id="' . $container_id . '"';
+
+  				if ( $container_class )
+  					$fb_output .= ' class="' . $container_class . '"';
+
+  				$fb_output .= '>';
+  			}
+
+  			$fb_output .= '<ul';
+
+  			if ( $menu_id )
+  				$fb_output .= ' id="' . $menu_id . '"';
+
+  			if ( $menu_class )
+  				$fb_output .= ' class="' . $menu_class . '"';
+
+  			$fb_output .= '>';
+  			$fb_output .= '<li><a href="' . admin_url( 'nav-menus.php' ) . '">Add a menu</a></li>';
+  			$fb_output .= '</ul>';
+
+  			if ( $container )
+  				$fb_output .= '</' . $container . '>';
+
+  			echo $fb_output;
+  		}
+  	}
+  }
+  // END bootstrap dropdown customisation
+
+
 // Breadcrumbs - see https://github.com/rachelbaker/bootstrapwp-Twitter-Bootstrap-for-WordPress
 
 function bootstrapwp_breadcrumbs() {
@@ -274,5 +483,60 @@ add_action('wp_print_styles', 'remove_all_jp_css' );
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
+// REMOVE ALL THE comments
+// Disable support for comments and trackbacks in post types
+function df_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ($post_types as $post_type) {
+		if(post_type_supports($post_type, 'comments')) {
+			remove_post_type_support($post_type, 'comments');
+			remove_post_type_support($post_type, 'trackbacks');
+		}
+	}
+}
+add_action('admin_init', 'df_disable_comments_post_types_support');
+
+// Close comments on the front-end
+function df_disable_comments_status() {
+	return false;
+}
+add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+
+// Hide existing comments
+function df_disable_comments_hide_existing_comments($comments) {
+	$comments = array();
+	return $comments;
+}
+add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+
+// Remove comments page in menu
+function df_disable_comments_admin_menu() {
+	remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'df_disable_comments_admin_menu');
+
+// Redirect any user trying to access comments page
+function df_disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ($pagenow === 'edit-comments.php') {
+		wp_redirect(admin_url()); exit;
+	}
+}
+add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+
+// Remove comments metabox from dashboard
+function df_disable_comments_dashboard() {
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'df_disable_comments_dashboard');
+
+// Remove comments links from admin bar
+function df_disable_comments_admin_bar() {
+	if (is_admin_bar_showing()) {
+		remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	}
+}
+add_action('init', 'df_disable_comments_admin_bar');
 
 ?>
