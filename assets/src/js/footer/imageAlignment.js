8bf7d1c449dc1ad2840f01e161057ed9f8498fc6
Ben Read <endymion1818@gmail.com>
2016-04-29 16:28:03 +0100

    fixes from a new site im building

--- assets/src/js/footer/imageAlignment.js
+++
@@ -1,7 +0,0 @@
-// This fixes responsive image problem with 'aligncenter' in Wordpress
-document.addEventListener('DOMContentLoaded',function()
-{
-    var elems = document.getElementsByClassName("aligncenter");
-    for (var i = 0; i < elems.length; i++)
-        elems.item(i).parentNode.classList.add('aligncenter');
-});
